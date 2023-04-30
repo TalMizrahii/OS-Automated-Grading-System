91bd35a84cc1eb7da8c865f4cddad6afdee60df7
TalMizrahii <Taltalon1927@gmail.com>
2023-04-30 15:48:03 +0300

    not tested version after implementation of the similar and identical functions.

--- ex21.c
+++ ex21.c
@@ -6,15 +6,6 @@
 #define ERROR (-1)
 #define STANDARD_ERROR_FD 2
 
-/**
- * Closing two files.
- * @param fd1 The first file to close.
- * @param fd2 The second file to close.
- */
-void closeFiles(int fd1, int fd2) {
-    close(fd1);
-    close(fd2);
-}
 
 /**
  * An "strlen" like function to get the value of a string in chars.
@@ -31,6 +22,7 @@ int strLength(const char *str) {
     return count;
 }
 
+
 /**
  * A "perror" like function to write to the Standard error (diagnostic) output.
  * @param errorMessage The message to print.
@@ -39,6 +31,19 @@ void errorPrint(char *errorMessage) {
     int charNum = strLength(errorMessage);
     write(STANDARD_ERROR_FD, errorMessage, charNum * sizeof(char));
 }
+
+/**
+ * Closing two files.
+ * @param fd1 The first file to close.
+ * @param fd2 The second file to close.
+ */
+void closeFiles(int fd1, int fd2) {
+    if (close(fd1) <= ERROR || close(fd2) <= ERROR) {
+        errorPrint("CAN NOT CLOSE FILE");
+        exit(-1);
+    }
+}
+
 
 /**
  * Checking if the amount of arguments are valid to 3.
@@ -86,76 +91,78 @@ long readByteFile(int fd, char *ch) {
     exit(-1);
 }
 
-int isSpace(char ch) {
-    if (ch == '\n' || ch == ' ' || ch == '\r') {
-        return 1;
-    }
-    return 0;
-}
 
-int similarEnd(int fd, char ch) {
-    do {
-        if (isSpace(ch)) {
-            continue;
-        } else {
-            return 2;
-        }
-    } while (readByteFile(fd, &ch));
-    return 3;
-}
-
-
-int isLetter(char c1) {
-    if ((c1 >= 65 && c1 <= 90) || (c1 >= 97 && c1 <= 122)) {
-        return 1;
-    }
-    return 0;
-}
-
-
-int unequal(int fd1, int fd2, char ch1, char ch2) {
-    if(isSpace(ch1)){
-
-    }
-
-    if (isLetter(ch1) && isLetter(ch2)) {
-
-    }
-}
-
-
-/**
- * Reading both files and comparing char by char if the files are similar, identical or different.
- * @param fd1 The file descriptor of the first file.
- * @param fd2 The file descriptor of the second file.
- * @return 1 for identical, 3 for similar and 2 otherwise.
- */
-int readAndCompare(int fd1, int fd2) {
+int identical(int fd1, int fd2) {
     // Creating chars to store the reading from the files.
     char ch1, ch2;
     // Return values from the read.
     long x1, x2;
-    int similar = 0, identical = 1, different = 0;
     while (1) {
         // Read from the files one byte.
         x1 = readByteFile(fd1, &ch1);
         x2 = readByteFile(fd2, &ch2);
-        // If both file ended, check only for similarity flag and return it.
         if (x1 == 0 && x2 == 0) {
-            return similar ? 3 : 1;
+            closeFiles(fd1, fd2);
+            return 1;
         }
-        if (x1 == 0) {
-            return similarEnd(fd2, ch2);
+        if ((x1 == 0) || (x2 == 0) || (ch1 != ch2)) {
+            closeFiles(fd1, fd2);
+            return 0;
         }
-        if (x2 == 0) {
-            return similarEnd(fd1, ch1);
-        }
-//        if(unequal())
-
     }
-
 }
 
+int isSpace(char ch){
+    if(ch == ' ' || ch == '\n' || ch == '\r'){
+        return 1;
+    }
+    return 0;
+}
+
+char upper(char ch){
+    if(ch >= 65 && ch <= 90){
+        ch = (char)(ch + 32);
+    }
+    return ch;
+}
+
+int similar(int fd1, int fd2){
+    // Creating chars to store the reading from the files.
+    char ch1 = 0, ch2 = 0;
+    do{
+        readByteFile(fd1, &ch1);
+        readByteFile(fd2, &ch2);
+
+        while(isSpace(ch1)){
+            readByteFile(fd1, &ch1);
+        }
+        while(isSpace(ch2)) {
+            readByteFile(fd2, &ch2);
+        }
+        if(upper(ch1) != upper(ch2)){
+            closeFiles(fd1, fd2);
+            return 0;
+        }
+    } while(ch1 != EOF && ch2 != EOF);
+
+    if(ch1 == EOF && ch2 == EOF){
+        closeFiles(fd1, fd2);
+        return 1;
+    }
+    closeFiles(fd1, fd2);
+    return 0;
+}
+
+void openFiles(int *fd1, int *fd2, char *file1, char *file2) {
+    if ((*fd1 = openFile(file1)) <= ERROR || (*fd2 = openFile(file2)) <= ERROR) {
+        // Print error message.
+        errorPrint("CAN'T OPEN FILES");
+        // Close the files.
+        closeFiles(*fd1, *fd2);
+        // If the opening failed, return an error.
+        exit(-1);
+    }
+}
 
 /**
  * The main function of the program.
@@ -171,15 +178,15 @@ int main(int argc, char *argv[]) {
     // Create two integers for the file's file descriptors.
     int fd1, fd2;
     // Open the files.
-    if ((fd1 = openFile(argv[1])) <= ERROR || (fd2 = openFile(argv[2])) <= ERROR) {
-        // Print error message.
-        errorPrint("CAN'T OPEN FILES");
-        // Close the files.
-        closeFiles(fd1, fd2);
-        // If the opening failed, return an error.
-        exit(-1);
+    openFiles(&fd1, &fd2, argv[1], argv[2]);
+    if (identical(fd1, fd2)) {
+        return 1;
     }
-
-
-    return 0;
+    openFiles(&fd1, &fd2, argv[1], argv[2]);
+    if (similar(fd1, fd2)) {
+        return 3;
+    }
+    closeFiles(fd1, fd2);
+    return 2;
 }
+
